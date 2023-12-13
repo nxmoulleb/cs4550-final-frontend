0834c1f4d5ec497ac1d98442c4fe3c163d7b1c4d
nxmoulleb <nxmoulleb@gmail.com>
2023-12-13 04:50:44 -0500

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { HashRouter } from "react-router-dom";
-import { Routes, Route, Navigate } from "react-router";
+import { Routes, Route } from "react-router";
 import SignIn from "./project/signin";
 import Register from "./project/register";
 import Account from "./project/account";
